17d6911d5ef31c93f41fb6993ba7bc123c18a70a
Nadav Spitzer <nspitzern@gmail.com>
2022-06-22 21:28:21 +0300

    adding pre-loading of basic models' results

--- models.py
+++ models.py
@@ -8,55 +8,58 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 from xgboost import XGBClassifier, XGBRegressor
 
+import numpy as np
+
 from dnn import get_dnn_model, get_dnn_results, dnn_predict
 
-def get_models_results(df, target, test_size=0.2, verbose=True):
-    # split to train and test
-    print('Splitting to Train/Test...')
-    y = df[[target]]
-    X = df.drop([target], axis=1)
+def get_models_results(df, target, test_size=0.2, load_data=False, verbose=True):
+    if load_data:
+        results_map = np.load('basic_models_results.npy')
+    else:
+        # split to train and test
+        print('Splitting to Train/Test...')
+        y = df[[target]]
+        X = df.drop([target], axis=1)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-    
-    print(f'Train Size: X={X_train.shape}, Y={y_train.shape}')
-    print(f'Test Size: X={X_test.shape}, Y={y_test.shape}')
-    
-    # create models
-    print('Creating models...')
-    reg_model = LinearRegression()
-    reg_tree = DecisionTreeRegressor()
-    forest_reg = RandomForestRegressor(n_estimators=50, max_depth=5, n_jobs=5)
-    ada_reg = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators=50)
-    xg_reg_model = XGBRegressor()
-    dnn_model = get_dnn_model(len(X_train.columns))
-    
-    
-    # run each model
-    print('Running models...')
-    print('Running Linear Regression...')
-    reg_model = reg_model.fit(X_train, y_train[target])
-    print('Running Decision Tree...')
-    reg_tree = reg_tree.fit(X_train, y_train[target])
-    print('Running Random Forest...')
-    forest_reg = forest_reg.fit(X_train, y_train[target])
-    print('Running AdaBoosting...')
-    ada_reg = ada_reg.fit(X_train, y_train[target])
-    print('Running XGBoost')
-    xg_reg_model = xg_reg_model.fit(X_train, y_train[target])
-    print('Running Neural Network...')
-    train_losses, test_losses = get_dnn_results(X_train, X_test, y_train, y_test, dnn_model, verbose)
-    
-    # evaluate results
-    results_map = {
-            'lin_reg': _get_scores(reg_model, reg_model.predict, X_test, y_test, target),
-            'reg_tree': _get_scores(reg_tree, reg_tree.predict, X_test, y_test, target),
-            'forest_reg': _get_scores(forest_reg, forest_reg.predict, X_test, y_test, target),
-            'ada_reg': _get_scores(ada_reg, ada_reg.predict, X_test, y_test, target),
-            'xg_reg_model': _get_scores(xg_reg_model, xg_reg_model.predict, X_test, y_test, target),
-            'dnn_model': _get_scores(dnn_model, dnn_predict, X_test, y_test, target, dnn=True)
-    }
-    
-    print(results_map)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+
+        print(f'Train Size: X={X_train.shape}, Y={y_train.shape}')
+        print(f'Test Size: X={X_test.shape}, Y={y_test.shape}')
+
+        # create models
+        print('Creating models...')
+        reg_model = LinearRegression()
+        reg_tree = DecisionTreeRegressor()
+        forest_reg = RandomForestRegressor(n_estimators=50, max_depth=5, n_jobs=5)
+        ada_reg = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators=50)
+        xg_reg_model = XGBRegressor()
+        dnn_model = get_dnn_model(len(X_train.columns))
+
+
+        # run each model
+        print('Running models...')
+        print('Running Linear Regression...')
+        reg_model = reg_model.fit(X_train, y_train[target])
+        print('Running Decision Tree...')
+        reg_tree = reg_tree.fit(X_train, y_train[target])
+        print('Running Random Forest...')
+        forest_reg = forest_reg.fit(X_train, y_train[target])
+        print('Running AdaBoosting...')
+        ada_reg = ada_reg.fit(X_train, y_train[target])
+        print('Running XGBoost')
+        xg_reg_model = xg_reg_model.fit(X_train, y_train[target])
+        print('Running Neural Network...')
+        train_losses, test_losses = get_dnn_results(X_train, X_test, y_train, y_test, dnn_model, verbose)
+
+        # evaluate results
+        results_map = {
+                'lin_reg': _get_scores(reg_model, reg_model.predict, X_test, y_test, target),
+                'reg_tree': _get_scores(reg_tree, reg_tree.predict, X_test, y_test, target),
+                'forest_reg': _get_scores(forest_reg, forest_reg.predict, X_test, y_test, target),
+                'ada_reg': _get_scores(ada_reg, ada_reg.predict, X_test, y_test, target),
+                'xg_reg_model': _get_scores(xg_reg_model, xg_reg_model.predict, X_test, y_test, target),
+                'dnn_model': _get_scores(dnn_model, dnn_predict, X_test, y_test, target, dnn=True)
+        }
     
     return results_map
 
