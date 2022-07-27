5eb63833320275fc8bd226dfb745d849c63ea1af
Nadav Spitzer <nspitzern@gmail.com>
2022-07-27 22:49:35 +0300

    add display of dataframes side by side

--- visualization.py
+++ visualization.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+from IPython.display import display_html
+from itertools import chain,cycle
+
+
+def display_side_by_side(*args,titles=cycle([''])):
+    html_str=''
+    for df,title in zip(args, chain(titles,cycle(['</br>'])) ):
+        html_str+='<th style="text-align:center"><td style="vertical-align:top">'
+        html_str+=f'<h2 style="text-align: center;">{title}</h2>'
+        html_str+=df.to_html().replace('table','table style="display:inline"')
+        html_str+='</td></th>'
+    display_html(html_str,raw=True)
+
 
 def get_scores_matrics(results):
     scores = pd.DataFrame(data={'MSE': [results['lin_reg']['MSE'], results['reg_tree']['MSE'], results['forest_reg']['MSE'], results['ada_reg']['MSE'], results['xg_reg_model']['MSE'], results['dnn_model']['MSE']],
