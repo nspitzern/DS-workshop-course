3d8f88ca8a18cf26d64d7483c3bcbe453c9757fc
Nadav Spitzer <nspitzern@gmail.com>
2022-08-02 09:53:26 +0300

    adjust visualizations code to be easier for modifications

--- visualization.py
+++ visualization.py
@@ -15,10 +15,34 @@ def display_side_by_side(*args,titles=cycle([''])):
 
 
 def get_scores_matrics(results):
-    scores = pd.DataFrame(data={'MSE': [results['lin_reg']['MSE'], results['reg_tree']['MSE'], results['forest_reg']['MSE'], results['ada_reg']['MSE'], results['xg_reg_model']['MSE'], results['dnn_model']['MSE']],
-                           'RMSE': [results['lin_reg']['RMSE'], results['reg_tree']['RMSE'], results['forest_reg']['RMSE'], results['ada_reg']['RMSE'], results['xg_reg_model']['RMSE'], results['dnn_model']['RMSE']],
-                           'MAE': [results['lin_reg']['MAE'], results['reg_tree']['MAE'], results['forest_reg']['MAE'], results['ada_reg']['MAE'], results['xg_reg_model']['MAE'], results['dnn_model']['MAE']],
-                           'R^2': [results['lin_reg']['R^2'], results['reg_tree']['R^2'], results['forest_reg']['R^2'], results['ada_reg']['R^2'], results['xg_reg_model']['R^2'], results['dnn_model']['R^2']]
+    scores = pd.DataFrame(data={'MSE': [results['lin_reg']['MSE'], 
+                                        results['reg_tree']['MSE'], 
+                                        results['forest_reg']['MSE'], 
+                                        results['ada_reg']['MSE'], 
+                                        results['xg_reg_model']['MSE'], 
+                                        results['dnn_model']['MSE']
+                                       ],
+                           'RMSE': [results['lin_reg']['RMSE'], 
+                                    results['reg_tree']['RMSE'], 
+                                    results['forest_reg']['RMSE'], 
+                                    results['ada_reg']['RMSE'], 
+                                    results['xg_reg_model']['RMSE'], 
+                                    results['dnn_model']['RMSE']
+                                   ],
+                           'MAE': [results['lin_reg']['MAE'], 
+                                   results['reg_tree']['MAE'], 
+                                   results['forest_reg']['MAE'], 
+                                   results['ada_reg']['MAE'], 
+                                   results['xg_reg_model']['MAE'], 
+                                   results['dnn_model']['MAE']
+                                  ],
+                           'R^2': [results['lin_reg']['R^2'], 
+                                   results['reg_tree']['R^2'], 
+                                   results['forest_reg']['R^2'], 
+                                   results['ada_reg']['R^2'], 
+                                   results['xg_reg_model']['R^2'], 
+                                   results['dnn_model']['R^2']
+                                  ]
                             },
                      index=['Linear Regression', 'Decision Tree', 'Random Forest', 'AdaBoosting', 'XGBoost', 'NN'])
     return scores.style.apply(_highlight)
