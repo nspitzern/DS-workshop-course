712a615822757977436595d2634cfd6c8eb484e7
Nadav Spitzer <nspitzern@gmail.com>
2022-08-08 13:47:00 +0300

    add visualization of shap features importance

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-from xgboost import XGBClassifier, XGBRegressor
+from xgboost import XGBRegressor
 
 import numpy as np
 
--- visualization.py
+++ visualization.py
@@ -15,28 +15,32 @@ def display_side_by_side(*args,titles=cycle([''])):
 
 
 def get_scores_matrics(results):
-    scores = pd.DataFrame(data={'MSE': [results['lin_reg']['MSE'], 
+    scores = pd.DataFrame(data={'MSE': [
+                                        results['lin_reg']['MSE'], 
                                         results['reg_tree']['MSE'], 
                                         results['forest_reg']['MSE'], 
                                         results['ada_reg']['MSE'], 
                                         results['xg_reg_model']['MSE'], 
                                         results['dnn_model']['MSE']
                                        ],
-                           'RMSE': [results['lin_reg']['RMSE'], 
+                           'RMSE': [
+                                    results['lin_reg']['RMSE'], 
                                     results['reg_tree']['RMSE'], 
                                     results['forest_reg']['RMSE'], 
                                     results['ada_reg']['RMSE'], 
                                     results['xg_reg_model']['RMSE'], 
                                     results['dnn_model']['RMSE']
                                    ],
-                           'MAE': [results['lin_reg']['MAE'], 
+                           'MAE': [
+                                   results['lin_reg']['MAE'], 
                                    results['reg_tree']['MAE'], 
                                    results['forest_reg']['MAE'], 
                                    results['ada_reg']['MAE'], 
                                    results['xg_reg_model']['MAE'], 
                                    results['dnn_model']['MAE']
                                   ],
-                           'R^2': [results['lin_reg']['R^2'], 
+                           'R^2': [
+                                   results['lin_reg']['R^2'], 
                                    results['reg_tree']['R^2'], 
                                    results['forest_reg']['R^2'], 
                                    results['ada_reg']['R^2'], 
@@ -55,3 +59,14 @@ def _highlight(data):
     else:
         is_min = data == data.min()
         return ['background: lightgreen' if cell else '' for cell in is_min]
+    
+    
+def show_shap_feature_importance(shap_values, X):
+    vals = np.abs(shap_values.values).mean(0)
+    feature_names = X.columns
+
+    feature_importance = pd.DataFrame(list(zip(feature_names, vals)),
+                                     columns=['col_name','feature_importance_vals'])
+    feature_importance.sort_values(by=['feature_importance_vals'],
+                                  ascending=False, inplace=True)
+    feature_importance.head(50)
