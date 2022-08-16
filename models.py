cfaed4bed19c288869f90ac8f59bc7581defeccc
Nadav Spitzer <nspitzern@gmail.com>
2022-08-16 13:10:00 +0300

    remove pretrained models and results. remove load models and results. remove unncessary visualization

--- models.py
+++ models.py
@@ -12,60 +12,45 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
 
 
-def get_models(filepath='', verbose=False):
-    models = dict()
-    
-    if filepath != '':    
-        with open(filepath, 'rb') as f:
-            model = pickle.load(f)
-            
-            if verbose:
-                print('Model Loaded!')
-    else:
-        # create models
-        if verbose:
-            print('Creating models...')
-        model = LinearRegression()
-        
-        if verbose:
-            print('Model Created!')
+def get_models(verbose=False):
+    # create models
+    if verbose:
+        print('Creating models...')
+    model = LinearRegression()
+
+    if verbose:
+        print('Model Created!')
         
     return model
 
-def get_models_results(df, target, model=None, test_size=0.2, ignore_columns=None, load_data=False, data_file_path='', save_models=False, is_basic=False, verbose=True):
-    if load_data:
-        assert data_file_path != '', "Please provide path to the data"
-        
-        with open(data_file_path, 'r') as f:
-            results_map = json.load(f)
-    else:
-        # split to train and test
-        if verbose:
-            print('Splitting to Train/Test...')
-        ignore_columns = ignore_columns if ignore_columns is not None else []
-        
-        y = df[target]
-        X = df.drop([target] + ignore_columns, axis=1)
+def get_models_results(df, target, model=None, test_size=0.2, ignore_columns=None, save_models=False, is_basic=False, verbose=True):
+    # split to train and test
+    if verbose:
+        print('Splitting to Train/Test...')
+    ignore_columns = ignore_columns if ignore_columns is not None else []
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    y = df[target]
+    X = df.drop([target] + ignore_columns, axis=1)
 
-        if verbose:
-            print(f'Train Size: X={X_train.shape}, Y={y_train.shape}')
-            print(f'Test Size: X={X_test.shape}, Y={y_test.shape}')
-        
-        assert model is not None, 'Please provide model'
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
-        # run each model
-        if verbose:
-            print('Running model...')
-        model.fit(X_train, y_train)
-        
-        if save_models:
-            with open(f'{"basic_" if is_basic else ""}model.pickle', 'wb') as f:
-                pickle.dump(model, f, protocol=pickle.HIGHEST_PROTOCOL)
+    if verbose:
+        print(f'Train Size: X={X_train.shape}, Y={y_train.shape}')
+        print(f'Test Size: X={X_test.shape}, Y={y_test.shape}')
 
-        # evaluate results
-        results_map = _get_scores(model.predict, X_test, y_test)
+    assert model is not None, 'Please provide model'
+
+    # run model
+    if verbose:
+        print('Running model...')
+    model.fit(X_train, y_train)
+
+    if save_models:
+        with open(f'{"basic_" if is_basic else ""}model.pickle', 'wb') as f:
+            pickle.dump(model, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # evaluate results
+    results_map = _get_scores(model.predict, X_test, y_test)
     
     return results_map
 
--- visualization.py
+++ visualization.py
@@ -3,19 +3,6 @@ from collections import defaultdict
 import pandas as pd
 import numpy as np
 
-from IPython.display import display_html
-from itertools import chain,cycle
-
-
-def display_side_by_side(*args,titles=cycle([''])):
-    html_str=''
-    for df,title in zip(args, chain(titles,cycle(['</br>'])) ):
-        html_str+='<th style="text-align:center"><td style="vertical-align:top">'
-        html_str+=f'<h2 style="text-align: center;">{title}</h2>'
-        html_str+=df.to_html().replace('table','table style="display:inline"')
-        html_str+='</td></th>'
-    display_html(html_str,raw=True)
-
 
 def get_scores_matrics(results, *args):
     data = defaultdict(list)
@@ -36,14 +23,3 @@ def _highlight(data):
     else:
         is_min = data == data.min()
         return ['background: lightgreen' if cell else '' for cell in is_min]
-    
-    
-def show_shap_feature_importance(shap_values, X):
-    vals = np.abs(shap_values.values).mean(0)
-    feature_names = X.columns
-
-    feature_importance = pd.DataFrame(list(zip(feature_names, vals)),
-                                     columns=['col_name','feature_importance_vals'])
-    feature_importance.sort_values(by=['feature_importance_vals'],
-                                  ascending=False, inplace=True)
-    feature_importance.head(50)
